d1efabab3dc7b62c37f2d35db2f9ed908d750b1d
adminy <marinbivol10@gmail.com>
2023-02-08 13:56:30 +0000

    add few extra builtin tools

--- debos-recipes/scripts/install-standard.sh
+++ debos-recipes/scripts/install-standard.sh
@@ -7,3 +7,11 @@
 apt-get install $(dpkg-query -W -f'${Package}\t${Priority}\n' | awk '/important$/ {printf "%s ", $1}')
 # Install packages with priority 'standard'
 apt-get install $(dpkg-query -W -f'${Package}\t${Priority}\n' | awk '/standard$/ {printf "%s ", $1}')
+
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
